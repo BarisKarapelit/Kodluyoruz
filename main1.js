0316d2819d9f3a2e68723ed676b9454c208d871f
BarisKarapelit <baris_karapelit_298@hotmail.com>
2022-06-23 15:49:23 +0300

    Project main file is fix

--- main1.js
+++
@@ -1,7 +0,0 @@
-const groups =['OrangeRed','AliceBlue','DeepPink'];
-const fetchedData=fetch('./data.json').then((res)=>res.json().then(data=>data));
-
-groups.forEach((color)=>{
-    let group = fetchedData.filter(e => e.group == color)
-    console.log(group)
-})
